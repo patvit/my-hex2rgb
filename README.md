4a21830054deaba71d50c0628ab3742fb087ec6a
Vitaly Pateshman <vitalypateshman@MacBook-Pro-Vitaly.local>
2024-06-16 19:08:44 +0300

    my-hex2rgb

---
+++ README.md
@@ -0,0 +1,11 @@
+[![Build status](https://ci.appveyor.com/api/projects/status/45he4kg1tkvakp6a?svg=true)](https://ci.appveyor.com/project/patvit/my-hex2rgb)
+
+
+# React + Vite
+
+This template provides a minimal setup to get React working in Vite with HMR and some ESLint rules.
+
+Currently, two official plugins are available:
+
+- [@vitejs/plugin-react](https://github.com/vitejs/vite-plugin-react/blob/main/packages/plugin-react/README.md) uses [Babel](https://babeljs.io/) for Fast Refresh
+- [@vitejs/plugin-react-swc](https://github.com/vitejs/vite-plugin-react-swc) uses [SWC](https://swc.rs/) for Fast Refresh
---
+++ src/components/Converter.jsx
@@ -0,0 +1,41 @@
+import React, { useState } from 'react'
+
+function hex2rgb(hex) {
+  const result = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(hex);
+  return result
+     ? `rgb(${parseInt(result[1], 16)}, ${parseInt(result[2], 16)}, ${parseInt(result[3], 16)})`     
+     : null;
+}
+
+function Converter() {
+    const [rgb, setRgb] = useState('');
+    const errorColor = 'rgb(201, 082, 064)';
+    
+    const changeColor = (e) => {
+        const {value} = e.target;
+  
+        if (value.length === 7 && value[0] === '#' && hex2rgb(value)) {
+            setRgb(hex2rgb(value));
+            document.body.style.backgroundColor = value;
+        } else {
+            setRgb('Ошибка');
+            document.body.style.backgroundColor = errorColor;
+        }
+    }
+    return (
+        <form className='converter'>
+            <input 
+                className='input' 
+                type='text'
+                onChange={changeColor} 
+                maxLength={7} 
+                placeholder='цвет в формате HEX'
+            ></input>
+            <div className='output'>
+                {rgb}
+            </div>
+        </form>
+    )
+}
+
+export default Converter
